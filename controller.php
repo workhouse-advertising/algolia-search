627b77460dc1f19463c0e96a88137b5d78e5a23e
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2018-04-10 12:12:36 +0800

    Added initial setup of the search block

--- controller.php
+++ controller.php
@@ -2,14 +2,22 @@
 
 namespace Concrete\Package\AlgoliaSearch;
 
+use BlockType;
 use Concrete\Core\Job\Job;
 use Concrete\Core\Package\Package;
+use Concrete\Core\Support\Facade\Application;
+use Concrete\Core\Foundation\Service\ProviderList;
+
+use AlgoliaSearch\AlgoliaSearchProvider;
 
 class Controller extends Package
 {
     protected $pkgHandle = 'algolia_search';
     protected $appVersionRequired = '5.7.4';
     protected $pkgVersion = '1.0';
+    protected $pkgAutoloaderRegistries = [
+        'src' => '\AlgoliaSearch'
+    ];
 
     public function getPackageName()
     {
@@ -21,18 +29,21 @@ class Controller extends Package
         return t("Replaces the default Concrete5 search index with Algolia's");
     }
 
-    // public function getPackageAutoloaderRegistries()
+    // public function on_start()
     // {
-    //     return [
-    //         'src' => "\\PortlandLabs\\Elastic"
-    //     ];
+    //     if (!$this->app) {
+    //         $this->app = Application::getFacadeApplication();
+    //     }
+    //     $list = $this->app->make(ProviderList::class);
+    //     $list->registerProvider(AlgoliaSearchProvider::class);
     // }
 
-    // public function install()
-    // {
-    //     $pkg = parent::install();
-    //     $pkg->on_start();
-    //     $job = Job::installByPackage('index_algolia_search', $pkg);
-    //     $job = Job::installByPackage('index_algolia_search', $pkg);
-    // }
+    public function install()
+    {
+        $pkg = parent::install();
+        $pkg->on_start();
+        $job = Job::installByPackage('index_algolia_search', $pkg);
+        $job = Job::installByPackage('index_algolia_search', $pkg);
+        BlockType::installBlockTypeFromPackage('algolia_search', $pkg); 
+    }
 }
