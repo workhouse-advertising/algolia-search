bdc6bc504786ad16ce2717493a5cff26c4ba542d
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2018-04-10 17:06:28 +0800

    Added a header search block and tidied up some of the views

---
+++ blocks/algolia_search/templates/header_search.php
@@ -0,0 +1,19 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+    if (!isset($query) || !is_string($query)) {
+        $query = (string)$controller->get('query');
+    }
+?>
+<form action="<?=$view->url($resultTarget)?>" method="get" class="form-inline">
+    <?php if ($query === ''): ?>
+        <input name="search_paths[]" type="hidden" value="<?=htmlentities($baseSearchPath, ENT_COMPAT, APP_CHARSET) ?>" />
+    <?php elseif (isset($_REQUEST['search_paths']) && is_array($_REQUEST['search_paths'])): ?>
+        <?php foreach ($_REQUEST['search_paths'] as $search_path): ?>
+            <input name="search_paths[]" type="hidden" value="<?=htmlentities($search_path, ENT_COMPAT, APP_CHARSET) ?>" />
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <input class="form-control algolia-search-value" type="search" placeholder="Search entire site..." aria-label="Search" name="query" value="" id="headerSearchValue" />
+    <button class="btn btn-primary" type="submit"><span class="fa fa-search" aria-hidden="true"></span></button>
+</form>
+<?php if (isset($error) && $error): ?>
+    <?= $error; ?>
+<?php endif; ?>
--- blocks/algolia_search/view.php
+++ blocks/algolia_search/view.php
@@ -1,63 +1,77 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
-
-if (isset($error)) {
-    ?><?=$error?><br/><br/><?php
-
-}
-
+// use Config;
 if (!isset($query) || !is_string($query)) {
     $query = '';
 }
+?>
+<?php //// TODO: Add the Algalia search via NPM ?>
+<script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
 
-?><form action="<?=$view->url($resultTarget)?>" method="get" class="ccm-search-block-form"><?php
-    if (isset($title) && ($title !== '')) {
-        ?><h3><?=h($title)?></h3><?php
+<div id="algoliaResults-<?= $bID ?>">
+    <div class="input-group col-sm-12 col-md-4">
+        <input type="search" name="query" placeholder="Search entire site..." value="" class="form-control algolia-search-value" />
+    </div>
+    <div class="algolia-no-results" <?php if ($query): ?>style="display: none;"<?php endif; ?>>
+        <h4 style="margin-top:32px"><?=t('There were no results found. Please try another keyword or phrase.')?></h4>
+    </div>
+    <div class="algolia-searching" <?php if (!$query): ?>style="display: none;"<?php endif; ?>>
+        <h4 style="margin-top:32px"><span class="fa fa-refresh fa-spin"></span> <?=t('Searching for results...')?></h4>
+    </div>
+    <div class="algolia-search-results">
+    </div>
+</div>
 
-    }
-    if ($query === '') {
-        ?><input name="search_paths[]" type="hidden" value="<?=htmlentities($baseSearchPath, ENT_COMPAT, APP_CHARSET) ?>" /><?php
+<script type="text/template" role="algolia-result" id="algoliaResultTemplate-<?= $bID ?>">
+    <div class="searchResult">
+        <h3><a href="<%= path %>"><%= name %></a></h3>
+        <p>
+            <%= content %>
+            <br/>
+            <a href="<%= path %>" class="pageLink">
+                <%= path %>
+            </a>
+        </p>
+    </div>
+</script>
+<script type="text/javascript">
+    var algoliaClient = algoliasearch('<?= Config::get('algolia_search::algolia.application_id'); ?>', '<?= Config::get('algolia_search::algolia.search_api_key'); ?>');
+    var algoliaIndex = algoliaClient.initIndex('<?= Config::get('algolia_search::algolia.index_key'); ?>');
+    var algoliaResultsContainer, algoliaResultTemplate;
+    $(function() {
+        algoliaResultsContainer = $('#algoliaResults-<?= $bID ?> .algolia-search-results');
+        algoliaResultTemplate = _.template($('#algoliaResultTemplate-<?= $bID ?>').html());
+        var urlParams = _.object(_.compact(_.map(window.location.search.slice(1).split('&'), function(item) {  if (item) return item.split('='); })));
+        triggerAlgoliaSearch(urlParams.query);
+        $('.algolia-search-value').on('keyup', _.debounce(function(e) {
+            e.preventDefault();
+            triggerAlgoliaSearch($(this).val());
+        }, 100));
+    });
 
-    } elseif (isset($_REQUEST['search_paths']) && is_array($_REQUEST['search_paths'])) {
-        foreach ($_REQUEST['search_paths'] as $search_path) {
-            ?><input name="search_paths[]" type="hidden" value="<?=htmlentities($search_path, ENT_COMPAT, APP_CHARSET) ?>" /><?php
-
-        }
-    }
-    ?><input name="query" type="text" value="<?=htmlentities($query, ENT_COMPAT, APP_CHARSET)?>" class="ccm-search-block-text" /><?php
-    if (isset($buttonText) && ($buttonText !== '')) {
-        ?> <input name="submit" type="submit" value="<?=h($buttonText)?>" class="btn btn-default ccm-search-block-submit" /><?php
-
-    }
-
-    if (isset($do_search) && $do_search) {
-        if (count($results) == 0) {
-            ?><h4 style="margin-top:32px"><?=t('There were no results found. Please try another keyword or phrase.')?></h4><?php
-
-        } else {
-            $tt = Core::make('helper/text');
-            ?><div id="searchResults"><?php
-                foreach ($results as $r) {
-                    $currentPageBody = $this->controller->highlightedExtendedMarkup($r->getPageIndexContent(), $query);
-                    ?><div class="searchResult">
-                        <h3><a href="<?=$r->getCollectionLink()?>"><?=$r->getCollectionName()?></a></h3>
-                        <p><?php
-                            if ($r->getCollectionDescription()) {
-                                echo $this->controller->highlightedMarkup($tt->shortText($r->getCollectionDescription()), $query);
-                                ?><br/><?php
-
-                            }
-                            echo $currentPageBody;
-                            ?> <br/><a href="<?=$r->getCollectionLink()?>" class="pageLink"><?=$this->controller->highlightedMarkup($r->getCollectionLink(), $query)?></a>
-                        </p>
-                    </div><?php
-
+    function triggerAlgoliaSearch(query) {
+        $('.algolia-search-value').val(query);
+        algoliaIndex.search({
+                query: query
+            },
+            function searchDone(err, content) {
+                $(algoliaResultsContainer).html('');
+                $('#algoliaResults-<?= $bID ?> .algolia-searching').hide();
+                $('#algoliaResults-<?= $bID ?> .algolia-no-results').hide();
+                if (content.hits && content.hits.length > 0) {
+                    $(content.hits).each(function(index, hit){
+                        if (!hit.content || hit.content == '') {
+                            hit.content = (hit.description) ? hit.description : '';
+                        }
+                        var length = Math.min(hit.content.length, 300);
+                        var suffix = (hit.content.length > 100) ? '...' : '   ';
+                        hit.content = hit.content.substring(0, length);
+                        $(algoliaResultsContainer).append(algoliaResultTemplate(hit));
+                    });
+                } else {
+                    $('#algoliaResults-<?= $bID ?> .algolia-no-results').show();
                 }
-            ?></div><?php
-            $pages = $pagination->getCurrentPageResults();
-            if ($pagination->haveToPaginate()) {
-                $showPagination = true;
-                echo $pagination->renderDefaultView();
+                if (err) throw err;
             }
-        }
+        );
     }
-?></form><?php
+</script>
