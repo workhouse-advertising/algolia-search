aeb2ee1de130713158676fc34c9ed17e27005e68
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2018-04-12 11:36:30 +0800

    Added indexing for FAQs - NOTE: This is very rough and was only done quickly due to extreme time constraints

--- jobs/index_algolia_search.php
+++ jobs/index_algolia_search.php
@@ -1,6 +1,8 @@
 <?php
 namespace Concrete\Package\AlgoliaSearch\Job;
 
+use Block;
+use BlockType;
 use Config;
 use Database;
 use Exception;
@@ -24,6 +26,12 @@ class IndexAlgoliaSearch extends AbstractJob
         return t("Generates the Algolia search index.");
     }
 
+    /**
+     * //// TODO: Must improve this whole package so it just adds a new indexing service. Have done it this way for 
+     *            now due to very tight time constraints.
+     * 
+     * @return [type] [description]
+     */
     public function run()
     {
         // Set up the Algolia client and index
@@ -36,21 +44,70 @@ class IndexAlgoliaSearch extends AbstractJob
         }
         // Fetch all current index records and add them to Algolia
         $indexedPages = Database::connection()->fetchAll('SELECT * from PageSearchIndex');
-        $validContentIds = [];
-        $deleteFilters = [];
         $errors = [];
         $successes = 0;
+
+        // Get the FAQ block type
+        $faqBlockType = BlockType::getByHandle('faq');
+        $validFaqIds = [];
+        if (is_object($faqBlockType)) {
+            // Get all active blocks for this block type
+            $faqBlocks = (array) Database::connection()->fetchAll("SELECT * FROM Blocks b 
+                                            WHERE bIsActive = 1
+                                            AND btID = ?
+                                            AND EXISTS (
+                                                SELECT 1 FROM CollectionVersionBlocks cvb
+                                                INNER JOIN CollectionVersions cv ON cv.cID=cvb.cID AND cv.cvID=cvb.cvID
+                                                WHERE b.bID=cvb.bID AND cv.cvIsApproved=1
+                                            )", [$faqBlockType->getBlockTypeID()]);
+            foreach ($faqBlocks as $faqBlock) {
+                $blockObject = Block::getByID($faqBlock['bID']);
+                // $blockPage = $blockObject->getOriginalCollection();
+                // Get the page that this individual block is on
+                $blockPage = $blockObject->getBlockCollectionObject();
+                // If it's a valid Page object, it's active and not a system page then index it
+                if ($blockPage && $blockPage instanceof Page && $blockPage->isActive() && !$blockPage->isPageDraft() && !$blockPage->isSystemPage()) {
+                    $pagePath = $blockPage->getCollectionPath();
+                    if ($pagePath) {
+                        // Fetch the FAQ entries for the specific block
+                        $faqEntiries = (array) Database::connection()->fetchAll("SELECT * FROM btFaqEntries WHERE bID = ?", [$faqBlock['bID']]);
+                        foreach ($faqEntiries as $faqEntry) {
+                            $validFaqIds[] = $faqEntry['id'];
+                            try {
+                                $this->algoliaIndex->saveObject([
+                                    'objectID' => "Faq::{$faqEntry['id']}",
+                                    'reference' => $faqEntry['id'],
+                                    'type' => 'faq',
+                                    'name' => $faqEntry['title'],
+                                    // Truncate to 10000 characters to account for Algolia's limits
+                                    'content' => substr($faqEntry['description'], 0, 10000),
+                                    'description' => '',
+                                    'path' => $pagePath,
+                                ]);
+                                $successes++;
+                            } catch (Exception $e) {
+                                $errors[] = $e->getMessage();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        $validPageIds = [];
         foreach ($indexedPages as $indexedPage) {
             // Don't show items whose paths contain '/!'
             $page = $this->getPage($indexedPage['cID']);
             // if (!stristr($indexedPage['cPath'], '/!')) {
             // if (!stristr($indexedPage['cPath'], '/!') && !preg_match('/^\/dashboard.*/', $indexedPage['cPath'])) {
             if ($indexedPage['cPath'] && trim($indexedPage['cName']) && $page && $page->isActive() && !$page->isPageDraft() && !$page->isSystemPage()) {
-                $validContentIds[] = $indexedPage['cID'];
-                $deleteFilters[] = "objectID:{$indexedPage['cID']}";
+                $validPageIds[] = $indexedPage['cID'];
                 try {
                     $this->algoliaIndex->saveObject([
-                        'objectID' => $indexedPage['cID'],
+                        'objectID' => "Page::{$indexedPage['cID']}",
+                        'reference' => $indexedPage['cID'],
+                        'type' => 'page',
                         'name' => $indexedPage['cName'],
                         // Truncate to 10000 characters to account for Algolia's limits
                         'content' => substr($indexedPage['content'], 0, 10000),
@@ -63,8 +120,13 @@ class IndexAlgoliaSearch extends AbstractJob
                 }
             }
         }
-        if ($validContentIds) {
-            $filters = 'NOT objectID:000' . implode(' AND NOT objectID:', $validContentIds);
+
+        if ($validPageIds) {
+            $filters = 'type:page AND NOT reference:' . implode(' AND NOT reference:', $validPageIds);
+            $this->algoliaIndex->deleteBy(['filters' => $filters]);
+        }
+        if ($validFaqIds) {
+            $filters = 'type:faq AND NOT reference:' . implode(' AND NOT reference:', $validFaqIds);
             $this->algoliaIndex->deleteBy(['filters' => $filters]);
         }
         $result = "{$successes} records successfully indexed.";
